6d2c88cbcf3bbbc53cf02e99dea244da0c6cec93
G.SANDEEP <137704913+GudisaSandeep@users.noreply.github.com>
2024-09-07 22:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -556,8 +556,8 @@ def get_conversation():
 
 
 
-if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
